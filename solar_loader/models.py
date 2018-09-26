a6e72b0fb535995b17886e7ab7c12bf8c570d5c6
nobohan <juminet@gmail.com>
2018-09-26 17:16:46 +0200

    add constantes with default values for the admin

--- solar_loader/models.py
+++ solar_loader/models.py
@@ -3,6 +3,158 @@ from jsonfield import JSONField
 from api.models import message_field
 
 
+def thermic_production_default():
+    """ return the default values as JSON """
+    return {
+       "60": {
+          "90": 519,
+          "112.5": 534,
+          "135": 546,
+          "157.5": 553,
+          "180": 555,
+          "202.5": 553,
+          "225": 546,
+          "247.5": 534,
+          "270": 519,
+       },
+       "90": {
+          "90": 742,
+          "112.5": 767,
+          "135": 787,
+          "157.5": 800,
+          "180": 804,
+          "202.5": 800,
+          "225": 787,
+          "247.5": 767,
+          "270": 742,
+       },
+       "120": {
+          "90": 932,
+          "112.5": 968,
+          "135": 997,
+          "157.5": 1015,
+          "180": 1022,
+          "202.5": 1015,
+          "225": 997,
+          "247.5": 968,
+          "270": 932,
+       },
+       "150": {
+          "90": 1097,
+          "112.5": 1145,
+          "135": 1183,
+          "157.5": 1207,
+          "180": 1215,
+          "202.5": 1207,
+          "225": 1183,
+          "247.5": 1145,
+          "270": 1097,
+       },
+       "180": {
+          "90": 1262,
+          "112.5": 1321,
+          "135": 1368,
+          "157.5": 1398,
+          "180": 1408,
+          "202.5": 1398,
+          "225": 1368,
+          "247.5": 1321,
+          "270": 1262,
+       },
+       "210": {
+          "90": 1364,
+          "112.5": 1432,
+          "135": 1487,
+          "157.5": 1522,
+          "180": 1534,
+          "202.5": 1522,
+          "225": 1487,
+          "247.5": 1432,
+          "270": 1364,
+       }
+    }
+
+def breakdown_cost_factor_default():
+    """ return the default values as JSON """
+    return {
+        'Belgium': {
+            'panels': 0.85,
+            'setup': 0.04,
+            'inverter': 0.09,
+            'transportBE': 0.02,
+            'transportEU': 0,
+            'transportBoat': 0
+        },
+        'Europe': {
+            'panels': 0.81,
+            'setup': 0.03,
+            'inverter': 0.08,
+            'transportBE': 0.02,
+            'transportEU': 0.05,
+            'transportBoat': 0
+        },
+        'China': {
+            'panels': 0.77,
+            'setup': 0.03,
+            'inverter': 0.08,
+            'transportBE': 0.02,
+            'transportEU': 0.02,
+            'transportBoat': 0.08
+        }
+    }
+
+
+def self_production_default():
+    """ return the default values as JSON """
+    return {
+        "default": {
+            "0.6": 0.25,
+            "1": 0.3,
+            "1.4": 0.34,
+            "1.8": 0.36,
+            "2.2": 0.38,
+            "2.6": 0.39,
+            "3": 0.4,
+        },
+        "energySobriety": {
+            "0.6": 0.28,
+            "1": 0.32,
+            "1.4": 0.35,
+            "1.8": 0.37,
+            "2.2": 0.38,
+            "2.6": 0.39,
+            "3": 0.4,
+        },
+        "chargeShift": {
+            "0.6": 0.3,
+            "1": 0.34,
+            "1.4": 0.36,
+            "1.8": 0.38,
+            "2.2": 0.39,
+            "2.6": 0.4,
+            "3": 0.41,
+        },
+        "pvHeater": {
+            "0.6": 0.43,
+            "1": 0.49,
+            "1.4": 0.54,
+            "1.8": 0.55,
+            "2.2": 0.57,
+            "2.6": 0.58,
+            "3": 0.6,
+        },
+        "battery": {
+            "0.6": 0.48,
+            "1": 0.54,
+            "1.4": 0.59,
+            "1.8": 0.62,
+            "2.2": 0.64,
+            "2.6": 0.66,
+            "3": 0.69,
+        }
+    }
+
+
 class SolarSim(models.Model):
     """
     A model to store and adjust solar-sim module constants
@@ -12,19 +164,72 @@ class SolarSim(models.Model):
     # we'll serve to maintain different configs and serve just one
     current = models.BooleanField(default=False)
 
-    # to encode simple values
-    max_power = models.FloatField()
-    co2_emissions_by_kwh = models.FloatField()
+    # Environmental
+    co2_emissions_by_kwh = models.FloatField(default=0.456)
+    energetic_cost_factor_belgium = models.FloatField(default=2500)
+    energetic_cost_factor_europe = models.FloatField(default=2600)
+    energetic_cost_factor_china = models.FloatField(default=2750)
+    breakdown_cost_factor = JSONField(default=breakdown_cost_factor_default)
 
-    # to encode simple stucts
-    energetic_cost_factor_belgium = models.FloatField()
-    energetic_cost_factor_europe = models.FloatField()
-    energetic_cost_factor_china = models.FloatField()
+    # financial
+    meter_cost = models.FloatField(default=289)
+    onduleur_cost_factor = models.FloatField(default=250)
+    onduleur_replacement_rate = models.FloatField(default=15)
+    redevance_cost = models.FloatField(default=65)
+    inflation_rate = models.FloatField(default=0.02)
+    elec_buying_price = models.FloatField(default=0.23)
+    elec_index = models.FloatField(default=0.03)
+    discount_rate = models.FloatField(default=0.04)
+    cv_rate_switch_power = models.FloatField(default=5)
+    cv_rate_low_power = models.FloatField(default=3)
+    cv_rate_high_power = models.FloatField(default=2.4)
+    cv_time = models.FloatField(default=10)
+    cv_end_of_compensation_year = models.FloatField(default=2020)
+    production_yearly_loss_index = models.FloatField(default=0.0005)
+    maintenance_cost_factor = models.FloatField(default=0.0075)
 
-    # to encode complex structures
-    thermic_production = JSONField()
+    # roof
+    max_power = models.FloatField(default=12)
+    max_solar_productivity = models.FloatField(default=1300)
+    flat_roof_tilt = models.FloatField(default=5)
+    low_productivity_limit = models.FloatField(default=800)
+    pv_yield_poly = models.FloatField(default=0.13)
+    pv_yield_mono = models.FloatField(default=0.155)
+    pv_yield_mono_high = models.FloatField(default=0.22)
+    pv_cost_poly = models.FloatField(default=1400/1.06)
+    pv_cost_mono = models.FloatField(default=1500/1.06)
+    pv_cost_mono_high = models.FloatField(default=1600/1.06)
 
-    # etc.
+    # user
+    annual_consumption_base = models.FloatField(default=600)
+    washing_machine_factor = models.FloatField(default=600)
+    electric_water_heater_factor = models.FloatField(default=2336)
+    electric_heating_factor = models.FloatField(default=16500)
+    self_production = JSONField(default=self_production_default)
+
+    # Thermic
+    thermic_installation_cost = models.FloatField(default=6000)
+    thermic_maintenance_cost = models.FloatField(default=100)
+    max_liter_per_day = models.FloatField(default=210)
+    min_thermic_area = models.FloatField(default=5)
+
+    hot_water_producer_yield_gas = models.FloatField(default=0.70)
+    hot_water_producer_yield_fuel = models.FloatField(default=0.55)
+    hot_water_producer_yield_electric = models.FloatField(default=0.95)
+
+    hot_water_energy_cost_gas = models.FloatField(default=0.080)
+    hot_water_energy_cost_fuel = models.FloatField(default=0.081)
+    hot_water_energy_cost_electric = models.FloatField(default=0.267)
+
+    hot_water_energy_cost_index_gas = models.FloatField(default=0.054)
+    hot_water_energy_cost_index_fuel = models.FloatField(default=0.099)
+    hot_water_energy_cost_index_electric = models.FloatField(default=0.040)
+
+    co2_emissions_by_kwh_thermic_gas = models.FloatField(default=0.201)
+    co2_emissions_by_kwh_thermic_fuel = models.FloatField(default=0.263)
+    co2_emissions_by_kwh_thermic_electric = models.FloatField(default=0.456)
+
+    thermic_production = JSONField(default=thermic_production_default)
 
 
 class AdjustDescription(models.Model):
