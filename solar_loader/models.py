c415b718525fb6949b2c2edbd005b85eb3593063
nobohan <juminet@gmail.com>
2018-09-27 22:30:20 +0200

    add help_text for solar_sim admin

--- solar_loader/models.py
+++ solar_loader/models.py
@@ -171,68 +171,239 @@ class SolarSim(models.Model):
         default=0.456,
         help_text='CO2 emissions by kWh of electric energy, in kg/kWh'
     )
-    energetic_cost_factor_belgium = models.FloatField(default=2500)
-    energetic_cost_factor_europe = models.FloatField(default=2600)
-    energetic_cost_factor_china = models.FloatField(default=2750)
-    breakdown_cost_factor = JSONField(default=breakdown_cost_factor_default)
+    energetic_cost_factor_belgium = models.FloatField(
+        default=2500,
+        help_text='Energetic cost for a photovoltaic installation, by origin \
+        of the technology, in kWh/kWc'
+    )
+    energetic_cost_factor_europe = models.FloatField(
+        default=2600,
+        help_text='Energetic cost for a photovoltaic installation, by origin \
+        of the technology, in kWh/kWc'
+    )
+    energetic_cost_factor_china = models.FloatField(
+        default=2750,
+        help_text='Energetic cost for a photovoltaic installation, by origin \
+        of the technology, in kWh/kWc'
+    )
+    breakdown_cost_factor = JSONField(
+        default=breakdown_cost_factor_default,
+        help_text='Breakdown of the energetic cost for a photovoltaic \
+        installation, by origin of the technology.'
+    )
 
     # financial
-    meter_cost = models.FloatField(default=289)
-    onduleur_cost_factor = models.FloatField(default=250)
-    onduleur_replacement_rate = models.FloatField(default=15)
-    redevance_cost = models.FloatField(default=65)
-    inflation_rate = models.FloatField(default=0.02)
-    elec_buying_price = models.FloatField(default=0.23)
-    elec_index = models.FloatField(default=0.03)
-    discount_rate = models.FloatField(default=0.04)
-    cv_rate_switch_power = models.FloatField(default=5)
-    cv_rate_low_power = models.FloatField(default=3)
-    cv_rate_high_power = models.FloatField(default=2.4)
-    cv_time = models.FloatField(default=10)
-    cv_end_of_compensation_year = models.FloatField(default=2020)
-    production_yearly_loss_index = models.FloatField(default=0.0005)
-    maintenance_cost_factor = models.FloatField(default=0.0075)
+    meter_cost = models.FloatField(
+        default=289,
+        help_text='Cost of the electric meter, in € (21% VAT included)'
+    )
+    onduleur_cost_factor = models.FloatField(
+        default=250,
+        help_text='Cost of the onduler, in € (21% VAT included)'
+    )
+    onduleur_replacement_rate = models.FloatField(
+        default=15,
+        help_text='Rate of onduler replacement, in years'
+    )
+    redevance_cost = models.FloatField(
+        default=65,
+        help_text='Cost of the redevance, in €'
+    )
+    inflation_rate = models.FloatField(
+        default=0.02,
+        help_text='Yearly inflation rate, in %'
+    )
+    elec_buying_price = models.FloatField(
+        default=0.23,
+        help_text='Buying electricity price, in €/kWh'
+    )
+    elec_index = models.FloatField(
+        default=0.03,
+        help_text='Yearly electricity price index, in %'
+    )
+    discount_rate = models.FloatField(
+        default=0.04,
+        help_text='Discount rate, in %'
+    )
+    cv_rate_switch_power = models.FloatField(
+        default=5,
+        help_text='Installation power (in KWc) at which the Certificat Vert \
+        rate change'
+    )
+    cv_rate_low_power = models.FloatField(
+        default=3,
+        help_text='Rate of Certificat Vert for low power installation, in \
+        CV/MWh'
+    )
+    cv_rate_high_power = models.FloatField(
+        default=2.4,
+        help_text='Rate of Certificat Vert for high power installation, in \
+        CV/MWh'
+    )
+    cv_time = models.FloatField(
+        default=10,
+        help_text='Duration of the Certicat Verts, in years'
+    )
+    cv_end_of_compensation_year = models.FloatField(
+        default=2020,
+        help_text='Year of the end of the compensation'
+    )
+    production_yearly_loss_index = models.FloatField(
+        default=0.0005,
+        help_text='Yearly photovoltaic production loss'
+    )
+    maintenance_cost_factor = models.FloatField(
+        default=0.0075,
+        help_text='Rate of maintenance cost with respect to the total \
+        photovoltaic installation price'
+    )
 
     # roof
-    max_power = models.FloatField(default=12)
-    max_solar_productivity = models.FloatField(default=1300)
-    flat_roof_tilt = models.FloatField(default=5)
-    low_productivity_limit = models.FloatField(default=800)
-    pv_yield_poly = models.FloatField(default=0.13)
-    pv_yield_mono = models.FloatField(default=0.155)
-    pv_yield_mono_high = models.FloatField(default=0.22)
-    pv_cost_poly = models.FloatField(default=1400/1.06)
-    pv_cost_mono = models.FloatField(default=1500/1.06)
-    pv_cost_mono_high = models.FloatField(default=1600/1.06)
+    max_power = models.FloatField(
+        default=12,
+        help_text='Maximum power of the photovoltaic installation, in kWc'
+    )
+    max_solar_productivity = models.FloatField(
+        default=1300,
+        help_text='Maximal solar productivity for a roof in Bruxelles, in \
+        kWh/m².an'
+    )
+    flat_roof_tilt = models.FloatField(
+        default=5,
+        help_text='Inclination (tilt) threshold for flat roof, in °. Roofs \
+        with a tilt below this threshold are considered as flat.'
+    )
+    low_productivity_limit = models.FloatField(
+        default=800,
+        help_text='Low limit for roof productivity (kWh/kWc.m²). No \
+        photovoltaic production is considered for roofs with a productivity \
+        below this limit.'
+    )
+    pv_yield_poly = models.FloatField(
+        default=0.13,
+        help_text='Yield of the photovoltaic system, for polycristallin panels'
+    )
+    pv_yield_mono = models.FloatField(
+        default=0.155,
+        help_text='Yield of the photovoltaic system, for monocristallin panels'
+    )
+    pv_yield_mono_high = models.FloatField(
+        default=0.22,
+        help_text='Yield of the photovoltaic system, for monocristallin high \
+        yield panels'
+    )
+    pv_cost_poly = models.FloatField(
+        default=1400/1.06,
+        help_text='Price of the photovoltaic panel, for polycristallin panels \
+        in €/kWc'
+    )
+    pv_cost_mono = models.FloatField(
+        default=1500/1.06,
+        help_text='Price of the photovoltaic panel, for monocristallin panels \
+        in €/kWc'
+    )
+    pv_cost_mono_high = models.FloatField(
+        default=1600/1.06,
+        help_text='Price of the photovoltaic panel, for high yield \
+        monocristallin panels in €/kWc'
+    )
 
     # user
-    annual_consumption_base = models.FloatField(default=600)
-    washing_machine_factor = models.FloatField(default=600)
-    electric_water_heater_factor = models.FloatField(default=2336)
-    electric_heating_factor = models.FloatField(default=16500)
-    self_production = JSONField(default=self_production_default)
+    annual_consumption_base = models.FloatField(
+        default=600,
+        help_text='Base of the annual consumption, in kWh'
+    )
+    washing_machine_factor = models.FloatField(
+        default=600,
+        help_text='Annual consumption of a washmachine, in kWh'
+    )
+    electric_water_heater_factor = models.FloatField(
+        default=2336,
+        help_text='Annual consumption of a hot water heater, in kWh'
+    )
+    electric_heating_factor = models.FloatField(
+        default=16500,
+        help_text='Annual consumption of a heating system, in kWh'
+    )
+    self_production = JSONField(
+        default=self_production_default,
+        help_text='Table of self production rates, for different \
+        user energetic profiles and self-production ratio'
+    )
 
     # Thermic
-    thermic_installation_cost = models.FloatField(default=6000)
-    thermic_maintenance_cost = models.FloatField(default=100)
-    max_liter_per_day = models.FloatField(default=210)
-    min_thermic_area = models.FloatField(default=5)
+    thermic_installation_cost = models.FloatField(
+        default=6000,
+        help_text='Average cost of a solar thermic installation, in € (VAT \
+        excluded)'
+    )
+    thermic_maintenance_cost = models.FloatField(
+        default=100,
+        help_text='Average maintenance cost of a solar thermic installation, \
+        in € (VAT excluded), applied to a given maintenance rate (3 years)'
+    )
+    max_liter_per_day = models.FloatField(
+        default=210,
+        help_text='Maximum hot water consumption per day, in liter'
+    )
+    min_thermic_area = models.FloatField(
+        default=5,
+        help_text='Minimal area for a solar thermic installation, in m². Below \
+        this limit, no solar thermic installation is possible'
+    )
 
-    hot_water_producer_yield_gas = models.FloatField(default=0.70)
-    hot_water_producer_yield_fuel = models.FloatField(default=0.55)
-    hot_water_producer_yield_electric = models.FloatField(default=0.95)
+    hot_water_producer_yield_gas = models.FloatField(
+        default=0.70,
+        help_text='Yield of the hot water production system with gas'
+    )
+    hot_water_producer_yield_fuel = models.FloatField(
+        default=0.55,
+        help_text='Yield of the hot water production system with fuel'
+    )
+    hot_water_producer_yield_electric = models.FloatField(
+        default=0.95,
+        help_text='Yield of the hot water production system with electricity'
+    )
 
-    hot_water_energy_cost_gas = models.FloatField(default=0.080)
-    hot_water_energy_cost_fuel = models.FloatField(default=0.081)
-    hot_water_energy_cost_electric = models.FloatField(default=0.267)
+    hot_water_energy_cost_gas = models.FloatField(
+        default=0.080,
+        help_text='Energy cost (for hot water production) for gas, in €/KWh'
+    )
+    hot_water_energy_cost_fuel = models.FloatField(
+        default=0.081,
+        help_text='Energy cost (for hot water production) for fuel, in €/KWh'
+    )
+    hot_water_energy_cost_electric = models.FloatField(
+        default=0.267,
+        help_text='Energy cost (for hot water production) for electricity, in \
+        €/KWh'
+    )
 
-    hot_water_energy_cost_index_gas = models.FloatField(default=0.054)
-    hot_water_energy_cost_index_fuel = models.FloatField(default=0.099)
-    hot_water_energy_cost_index_electric = models.FloatField(default=0.040)
+    hot_water_energy_cost_index_gas = models.FloatField(
+        default=0.054,
+        help_text='Energy cost index for gas'
+    )
+    hot_water_energy_cost_index_fuel = models.FloatField(
+        default=0.099,
+        help_text='Energy cost index for fuel'
+    )
+    hot_water_energy_cost_index_electric = models.FloatField(
+        default=0.040,
+        help_text='Energy cost index for electricity'
+    )
 
-    co2_emissions_by_kwh_thermic_gas = models.FloatField(default=0.201)
-    co2_emissions_by_kwh_thermic_fuel = models.FloatField(default=0.263)
-    co2_emissions_by_kwh_thermic_electric = models.FloatField(default=0.456)
+    co2_emissions_by_kwh_thermic_gas = models.FloatField(
+        default=0.201,
+        help_text='CO2 emissions by kWh of gas, in kg/kWh'
+    )
+    co2_emissions_by_kwh_thermic_fuel = models.FloatField(
+        default=0.263,
+        help_text='CO2 emissions by kWh of fuel, in kg/kWh'
+    )
+    co2_emissions_by_kwh_thermic_electric = models.FloatField(
+        default=0.456,
+        help_text='CO2 emissions by kWh of electricity, in kg/kWh'
+    )
 
     thermic_production = JSONField(default=thermic_production_default)
 
