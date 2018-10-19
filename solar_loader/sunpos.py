a3a3b4d3d333ee8d1077f875372b1db1a8c6608e
Marc Ducobu <marc.ducobu@champs-libres.coop>
2018-10-19 15:01:06 +0200

    Tests for sunpos

--- solar_loader/sunpos.py
+++ solar_loader/sunpos.py
@@ -2,18 +2,27 @@ import numpy as np
 from pysolar import solar
 from pyproj import Proj, transform
 from pytz import utc
-
+import logging
 from .records import SunPosition
-from .geom import vec2_dist
+
 
 l72 = Proj(init='EPSG:31370')
 wgs = Proj(init='EPSG:4326')
 
+logger = logging.getLogger(__name__)
 
 def _get_coords_from_angles(ref_point, elev, azimut, dist=10000):
+    """
+    Returns the position (x, y, z) :
+
+    - ref_point is the reference point
+    - elev : the elevation angle (in radians)
+    - azimut : the azimut angle (in radians - clockwise from north)
+    - dist : the distance (in meters)
+    """
     px, py, pz = ref_point
 
-    x = px + (dist * np.sin(azimut))
+    x = px + (dist * np.sin(-azimut))
     y = py + (dist * np.cos(azimut))
     z = pz + (dist * np.sin(elev))
 
@@ -36,6 +45,20 @@ def get_sun_position(ref_point, tim):
     azimut = solar.get_azimuth(lat, lon, utc_time, z)
     altitude = solar.get_altitude(lat, lon, utc_time, z)
     elev = altitude
+
+    # QUESTION PIERRE :
+    # besoin de vérifier < 0 et > 360 : indicateur mauvais input ?
+    if azimut < 0:
+        logger.error('get_azimuth({}, {}, {}, {}) is negative : {}'.format(
+            lat, lon, utc_time, z, azimut
+        ))
+    elif azimut > 360:
+        logger.error('get_azimuth({}, {}, {}, {}) is over 360 : {}'.format(
+            lat, lon, utc_time, z, azimut
+        ))
+
+    azimut_0_360 = azimut % 360 #  modulo (retour nombre entre 0 et 359.9)
+
     aa = abs(azimut)
     saa = aa if aa < 180 else aa - 360
 
---
+++ test/test_sunpos.py
@@ -0,0 +1,40 @@
+import unittest
+import math
+from solar_loader import sunpos
+from numpy.testing import assert_array_equal
+import numpy as np
+
+
+class TestSunPos(unittest.TestCase):
+    def test_inverse_azimut(self):
+        """ Test the inverse of the azimut"""
+        verif = [
+            # (azimut (degree), x, y)
+            (0, 0, 1),
+            (90, -1, 0),
+            (180, 0, -1),
+            (270, 1, 0),
+        ]
+
+        for v in verif:
+            azimut = v[0]
+            x_to_check = v[1]
+            y_to_check = v[2]
+            res = sunpos._get_coords_from_angles((0, 0, 0), 0, np.deg2rad(azimut), 1)
+            self.assertAlmostEqual(res[0], x_to_check)
+            self.assertAlmostEqual(res[1], y_to_check)
+
+    def test_get_sun_position(self):
+        self.skipTest('ToDo')
+
+    def test_azimuth_modulo_360(self):
+        self.assertEqual(-1.5 % 360, 358.5)
+        self.assertEqual(-1 % 360, 359)
+        self.assertEqual(380 % 360, 20)
+        self.assertAlmostEqual(366.7 % 360, 6.7)
+        self.assertEqual(90 % 360, 90)
+        self.assertEqual(360 % 360, 0)
+        self.assertEqual(0 % 360, 0)
+
+if __name__ == '__main__':
+    unittest.main()
